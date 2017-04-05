5c49976c7cee15bbcbef8869185b3abd741316c3
Vincent Fiduccia <vincent@rancher.com>
2017-04-05 14:48:24 -0700

    17.03.x

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $(dirname $0)
-gsutil rsync -c -r -d -x "sync\.sh|\.git" . gs://releases.rancher.com/install-docker
+gsutil rsync -c -r -d -x "sync\.sh|\.git|\.DS_Store" . gs://releases.rancher.com/install-docker
