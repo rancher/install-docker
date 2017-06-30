ac471689e9fcdc4cac88403577735fa91b14cada
Vincent Fiduccia <vincent@rancher.com>
2017-06-30 09:48:42 -0700

    Remove comments pointing to the wrong place

--- 17.03.2.sh
+++ 17.03.2.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -e
 
-# This script is meant for quick & easy install via:
-#   $ curl -fsSL get.docker.com -o get-docker.sh
-#   $ sh get-docker.sh
-#
-# For test builds (ie. release candidates):
-#   $ curl -fsSL test.docker.com -o test-docker.sh
-#   $ sh test-docker.sh
-#
-# NOTE: Make sure to verify the contents of the script
-#       you downloaded matches the contents of install.sh
-#       located at https://github.com/docker/docker-install
-#       before executing.
-
 CHANNEL="stable"
 
 docker_version=17.03.2
