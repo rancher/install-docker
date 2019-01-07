62dc17ba0dbb9dcea7cc69dda4c39119a57db3ba
Sebastiaan van Steenis <mail@superseb.nl>
2019-01-07 21:30:44 +0100

    Remove ce suffix for EL based distro

--- 18.09.0.sh
+++ 18.09.0.sh
@@ -405,7 +405,7 @@ do_install() {
 					$sh_c "$config_manager $enable_channel_flag docker-ce-$CHANNEL"
 				fi
 				$sh_c "$pkg_manager makecache fast"
-				$sh_c "$pkg_manager install -y -q docker-ce-${docker_version}.ce"
+				$sh_c "$pkg_manager install -y -q docker-ce-${docker_version}"
 				if [ -d '/run/systemd/system' ]; then
 					$sh_c 'service docker start'
 				else
