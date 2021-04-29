4766939cf8305c1053356d04b9e8034530ec2838
Sebastiaan van Steenis <mail@superseb.nl>
2021-04-29 18:05:31 +0200

    Add 20.10.6

---
+++ pkg/20.10.6/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="7cae5f8b0decc17d6571f9f52eb840fbc13b2737"
+
+TEST_OS_IMAGE_NAME=(ubuntu centos debian oraclelinux)
+TEST_OS_IMAGE_TAG[0]="16.04 18.04 20.04"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="10"
+TEST_OS_IMAGE_TAG[3]="7 8"
