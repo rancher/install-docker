76092ac557547adae93fdc39078eb2a086f538ed
Sebastiaan van Steenis <mail@superseb.nl>
2021-05-04 17:13:19 +0200

    Extend tested Docker versions

--- pkg/20.10.2/config.sh
+++ pkg/20.10.2/config.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-TEST_OS_IMAGE_NAME=(oraclelinux centos debian ubuntu)
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu)
 TEST_OS_IMAGE_TAG[0]="7 8"
 TEST_OS_IMAGE_TAG[1]="centos7 centos8"
-TEST_OS_IMAGE_TAG[2]="10"
-TEST_OS_IMAGE_TAG[3]="16.04 18.04 20.04"
+TEST_OS_IMAGE_TAG[2]="16.04 18.04 20.04"
 
 DOCKER_GIT_COMMIT="3d8fe77c2c46c5b7571f94b42793905e5b3e42e4"
