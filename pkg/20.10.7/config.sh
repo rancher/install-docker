2184d61c48b3ee52bb5fb74357352e72b2c8266e
Jacob Payne <jacob.payne@suse.com>
2021-06-07 23:36:40 +0000

    added installer for docker 20.10.7

---
+++ pkg/20.10.7/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="7cae5f8b0decc17d6571f9f52eb840fbc13b2737"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="16.04 18.04 20.04"
