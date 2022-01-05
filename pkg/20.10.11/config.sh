8b27e62ff04551a3e93a4f587b4afacb5f2c4c8d
Rayan Das <rayandas91@gmail.com>
2022-01-05 18:18:06 +0530

    added installer for docker 20.10.11

---
+++ pkg/20.10.11/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="93d2499759296ac1f9c510605fef85052a2c32be"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="18.04 20.04"
