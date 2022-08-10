529f0187c408063ddb6d4bbc49957aa51c2429da
mitulshah-suse <mitul.shah@suse.com>
2022-08-10 19:02:38 +0530

    add install docker scripts 20.10.13-17 and configs

---
+++ pkg/20.10.10/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="93d2499759296ac1f9c510605fef85052a2c32be"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="16.04 18.04 20.04"
+TEST_OS_IMAGE_TAG[3]="8"
