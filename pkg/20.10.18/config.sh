3527b74850c6653514cfc1f89af8123d6b121ce5
vardhaman <vardhaman.surana@suse.com>
2022-10-21 17:13:36 +0530

    added 20.10.18, 20.10.19, 20.10.20, 20.10.21 releases

---
+++ pkg/20.10.18/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="4f282167c425347a931ccfd95cc91fab041d414f"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="18.04 20.04 22.04"
+TEST_OS_IMAGE_TAG[3]="8"
