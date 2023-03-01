90a0ebf999088da0b051ee35e718843898629562
Vaishnav Gaikwad <vaishnavgaikwad721@gmail.com>
2023-03-01 18:11:48 +0530

    add docker 23.0.1

---
+++ pkg/23.0.1/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="66474034547a96caa0a25be56051ff8b726a1b28"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="18.04 20.04 22.04"
+TEST_OS_IMAGE_TAG[3]="8"
