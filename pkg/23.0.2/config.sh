46ecb29aa710dfbe9eecf64e4d5721b971184efc
mitulshah-suse <mitul.shah@suse.com>
2023-03-30 13:35:15 +0530

    Add 23.0.2 version and fix some link issues

---
+++ pkg/23.0.2/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="93bb55b16a5f5c8ce33e0f4784cb8af149d9590f"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="18.04 20.04 22.04"
+TEST_OS_IMAGE_TAG[3]="8"
