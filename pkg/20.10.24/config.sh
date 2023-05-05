ac6534a6f9712f81d8f9b5dc9928748b05a188bc
Vaishnav Gaikwad <vaishnavgaikwad721@gmail.com>
2023-05-05 22:10:44 +0530

    add docker 20.10.24

---
+++ pkg/20.10.24/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="a8a6b338bdfedd7ddefb96fe3e7fe7d4036d945a"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="18.04 20.04 22.04"
+TEST_OS_IMAGE_TAG[3]="8"
