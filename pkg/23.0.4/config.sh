1c5432fe40006da91115a083911aa0cf70057482
Vaishnav Gaikwad <vaishnavgaikwad721@gmail.com>
2023-05-11 16:23:00 +0530

    add rhel9 to test-scripts

--- pkg/23.0.4/config.sh
+++ pkg/23.0.4/config.sh
@@ -2,8 +2,9 @@
 
 DOCKER_GIT_COMMIT="a8a6b338bdfedd7ddefb96fe3e7fe7d4036d945a"
 
-TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
-TEST_OS_IMAGE_TAG[0]="7 8"
-TEST_OS_IMAGE_TAG[1]="centos7 centos8"
-TEST_OS_IMAGE_TAG[2]="18.04 20.04 22.04"
-TEST_OS_IMAGE_TAG[3]="8"
+TEST_OS_IMAGE_NAME=(redhat/ubi9 oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="9.0.0"
+TEST_OS_IMAGE_TAG[1]="7 8"
+TEST_OS_IMAGE_TAG[2]="centos7 centos8"
+TEST_OS_IMAGE_TAG[3]="18.04 20.04 22.04"
+TEST_OS_IMAGE_TAG[4]="8"
