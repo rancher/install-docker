4f22c8bf60ca52d664a97564d246c9683bbecc33
Chirayu Kapoor <chirayu.kapoor@suse.com>
2024-04-23 13:08:19 +0530

    chore: Update config.sh to default OSes and tags
    Signed-off-by: Chirayu Kapoor <chirayu.kapoor@suse.com>

--- pkg/23.0.6/config.sh
+++ pkg/23.0.6/config.sh
@@ -2,9 +2,11 @@
 
 DOCKER_GIT_COMMIT="a8a6b338bdfedd7ddefb96fe3e7fe7d4036d945a"
 
-TEST_OS_IMAGE_NAME=(redhat/ubi9 oraclelinux centos ubuntu rockylinux)
-TEST_OS_IMAGE_TAG[0]="9.0.0"
-TEST_OS_IMAGE_TAG[1]="7 8"
-TEST_OS_IMAGE_TAG[2]="centos7 centos8"
-TEST_OS_IMAGE_TAG[3]="18.04 20.04 22.04"
-TEST_OS_IMAGE_TAG[4]="8"
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux redhat/ubi9 quay.io/centos/centos redhat/ubi8)
+TEST_OS_IMAGE_TAG[0]="8 9"
+TEST_OS_IMAGE_TAG[1]="centos7"
+TEST_OS_IMAGE_TAG[2]="22.04"
+TEST_OS_IMAGE_TAG[3]="8 9"
+TEST_OS_IMAGE_TAG[4]="9.3"
+TEST_OS_IMAGE_TAG[5]="stream8"
+TEST_OS_IMAGE_TAG[6]="8.9"
--- pkg/24.0.2/config.sh
+++ pkg/24.0.2/config.sh
@@ -2,10 +2,11 @@
 
 DOCKER_GIT_COMMIT="c2de0811708b6d9015ed1a2c80f02c9b70c8ce7b"
 
-TEST_OS_IMAGE_NAME=(redhat/ubi9 oraclelinux centos ubuntu rockylinux quay.io/centos/centos)
-TEST_OS_IMAGE_TAG[0]="9.0.0"
-TEST_OS_IMAGE_TAG[1]="7 8"
-TEST_OS_IMAGE_TAG[2]="centos7"
-TEST_OS_IMAGE_TAG[3]="18.04 20.04 22.04"
-TEST_OS_IMAGE_TAG[4]="8"
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux redhat/ubi9 quay.io/centos/centos redhat/ubi8)
+TEST_OS_IMAGE_TAG[0]="8 9"
+TEST_OS_IMAGE_TAG[1]="centos7"
+TEST_OS_IMAGE_TAG[2]="22.04"
+TEST_OS_IMAGE_TAG[3]="8 9"
+TEST_OS_IMAGE_TAG[4]="9.3"
 TEST_OS_IMAGE_TAG[5]="stream8"
+TEST_OS_IMAGE_TAG[6]="8.9"
