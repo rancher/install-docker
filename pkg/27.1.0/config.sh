c37b65b295d842b32d4d752949b3e7a9bbeb6145
Chirayu Kapoor <chirayu.kapoor@suse.com>
2024-07-29 14:24:50 +0530

    [TEST] replace centos repo with RHEL

---
+++ pkg/27.1.0/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="1ce4e39c9502b89728cdd4790a8c3895709e358d"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux redhat/ubi9 quay.io/centos/centos redhat/ubi8)
+TEST_OS_IMAGE_TAG[0]="8 9"
+TEST_OS_IMAGE_TAG[1]="centos7"
+TEST_OS_IMAGE_TAG[2]="22.04 24.04"
+TEST_OS_IMAGE_TAG[3]="8 9"
+TEST_OS_IMAGE_TAG[4]="9.3"
+TEST_OS_IMAGE_TAG[5]="stream8"
+TEST_OS_IMAGE_TAG[6]="8.9"
