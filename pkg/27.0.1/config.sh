e5d3708067a40442fe7f802c986372f22bbc2865
Chirayu Kapoor <chirayu.kapoor@suse.com>
2024-07-31 13:48:58 +0530

    remove centos7 | update centos8 to centos9

--- pkg/27.0.1/config.sh
+++ pkg/27.0.1/config.sh
@@ -2,11 +2,10 @@
 
 DOCKER_GIT_COMMIT="1ce4e39c9502b89728cdd4790a8c3895709e358d"
 
-TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux redhat/ubi9 quay.io/centos/centos redhat/ubi8)
+TEST_OS_IMAGE_NAME=(oraclelinux ubuntu rockylinux redhat/ubi9 quay.io/centos/centos redhat/ubi8)
 TEST_OS_IMAGE_TAG[0]="8 9"
-TEST_OS_IMAGE_TAG[1]="centos7"
-TEST_OS_IMAGE_TAG[2]="22.04 24.04"
-TEST_OS_IMAGE_TAG[3]="8 9"
-TEST_OS_IMAGE_TAG[4]="9.3"
-TEST_OS_IMAGE_TAG[5]="stream8"
-TEST_OS_IMAGE_TAG[6]="8.9"
+TEST_OS_IMAGE_TAG[1]="22.04"
+TEST_OS_IMAGE_TAG[2]="8 9"
+TEST_OS_IMAGE_TAG[3]="9.3"
+TEST_OS_IMAGE_TAG[4]="stream9"
+TEST_OS_IMAGE_TAG[5]="8.9"
